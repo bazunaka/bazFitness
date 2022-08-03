cee90c8ed3e9257cb54b36b50f2068a09c066198
bazunaka <bazunaka32@gmail.com>
2022-08-03 11:37:28 +0300

    create java project

---
+++ bazFitness/src/bazfitness/BazFitness.java
@@ -0,0 +1,16 @@
+package bazfitness;
+
+/**
+ *
+ * @author bazunaka
+ */
+public class BazFitness {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
