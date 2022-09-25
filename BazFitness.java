ecb1673cc44d4fdaa7fe468062fc6623713e838b
bazunaka-macbook <bazunaka32@gmail.com>
2022-09-25 20:30:50 +0300

    create parse

--- BazFitness.java
+++ BazFitness.java
@@ -1,5 +1,9 @@
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
+import java.util.Scanner;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,10 +13,44 @@ import Models.Database;
 
 public class BazFitness extends Application {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    /**
+     * @param args
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     * @throws IOException
+     */
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, IOException {
         Database.Connect();
 
-        Database.InsertDB();
+        Scanner file;
+        PrintWriter writer;
+
+        file = new Scanner(new File("Data/shoulders.txt"));
+        writer = new PrintWriter("Data/shoulders_new.txt");
+
+        while (file.hasNext()) {
+            String line = file.nextLine();
+            if (!line.isEmpty()) {
+                writer.write(line);
+                writer.write("\n");
+            }
+        }
+
+        file.close();
+        writer.close();
+
+        /*
+         * Try start Python script from Java.
+         * try {
+         * String cmd =
+         * "python3 /Users/andrejsuvorov/Documents/GitHub/bazFitness/bazFitness_parsing.py"
+         * ;
+         * Process p = new ProcessBuilder(cmd).start();
+         * p.waitFor();
+         * } catch (InterruptedException e) {
+         * e.printStackTrace();
+         * }
+         */
 
         launch(args);
     }
@@ -23,5 +61,6 @@ public class BazFitness extends Application {
 
         stage.setScene(new Scene(root));
         stage.show();
+
     }
 }
--- bazFitness_parsing.py
+++ bazFitness_parsing.py
@@ -1,9 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://sportwiki.to/Категория:Тренинг'
+url = 'http://sportwiki.to/Плечи_-_упражнения_и_особенности_тренировки'
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'lxml')
+quotes = soup.find_all('li')
 
-print(soup)
+file = open("Data/shoulders.txt", "w")
+for quote in quotes:
+    file.write(quote.text)
+file.close()
+
+'''internalLinks = [
+    a.get('href') for a in soup.find_all('a', class_='mainpage-content')
+    if a.get('href') and a.get('href').startswith('/')]
+
+file = open("Data/urls.txt", "w")
+for links in internalLinks:
+    file.write("http://sportwiki.to" + links)'''
