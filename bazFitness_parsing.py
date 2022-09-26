befaca0c717ac6b9597880f519e627bea93156e3
bazunaka-macbook <bazunaka32@gmail.com>
2022-09-26 11:00:08 +0300

    add new data and modify parser

--- bazFitness_parsing.py
+++ bazFitness_parsing.py
@@ -1,16 +1,25 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
 url = 'http://sportwiki.to/Плечи_-_упражнения_и_особенности_тренировки'
 response = requests.get(url)
 
-soup = BeautifulSoup(response.text, 'lxml')
+soup = BeautifulSoup(response.text, 'html.parser')
 quotes = soup.find_all('li')
+result_list = {'name': [], 'fk': []}
 
-file = open("Data/shoulders.txt", "w")
+for quote in quotes:
+    result_list['name'].append(quote.text)
+    result_list['fk'].append('2')
+
+df = pd.DataFrame(result_list)
+df.to_csv("Data/shoulders.csv")
+
+'''file = open("Data/shoulders.txt", "w")
 for quote in quotes:
     file.write(quote.text)
-file.close()
+file.close()'''
 
 '''internalLinks = [
     a.get('href') for a in soup.find_all('a', class_='mainpage-content')
