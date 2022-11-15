c65e9934ba568cb442860dc67779e348a59f9e7e
bazunaka <bazunaka32@gmail.com>
2022-11-15 18:00:47 +0300

    modify DB

--- Scripts/insert_wtp 3.sql
+++ Scripts/insert_wtp 3.sql
@@ -23,19 +23,7 @@ VALUES ((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Жим лежа с 
 ((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Тяга гантелей в наклоне'), 3, 3, 10, 3),
 ((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Подтягивания широким хватом'), 3, 4, 8, 3),
 ((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Тяга верхнего блока к груди обратным хватом'), 3, 3, 10, 3),
-!!!((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Тяга за голову на высоком блоке'), 3, 3, 12, 3),
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Тяга нижнего блока обратным хватом'), 3, 3, 15, 3),
+((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Жим гантелей на горизонтальной скамье'), 3, 3, 12, 3),
+((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Разведение гантелей с поднятым головным концом скамьи'), 3, 3, 15, 3);
 
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Приседания со штангой на плечах'), 2, 3, 10, 4),
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Жим ногами с узкой постановкой'), 2, 3, 10, 4),
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Становая тяга со штангой на прямых ногах'), 2, 3, 10, 4),
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Сгибания ног в тренажере'), 2, 3, 10, 4),
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Подъем на носках сидя'), 2, 3, 10, 4),
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Шраги со штангой'), 2, 3, 10, 4),
-
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Армейский жим с гантелями'), 2, 3, 10, 6),
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Разведение рук с гантелями'), 2, 4, 8, 6),
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Тяга Т-грифа в тренажере'), 2, 4, 8, 6),
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Разгибание рук на трицепс с использованием канатной рукояти'), 2, 3, 12, 6),
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Сгибание рук с гантелями хватом «молоток»'), 2, 3, 12, 6),
-((SELECT WorkoutID FROM Workouts WHERE WorkoutName = 'Поочередное сгибание рук с гантелями'), 2, 3, 12, 6),
+'доделать тренировочный план через ПО - sportwiki.to/тренировочная программа 3 и 4 тренировки в неделю' 
