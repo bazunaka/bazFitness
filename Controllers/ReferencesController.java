cb2fbfcc8de421404bb326139aca40e6c9fb3b08
bazunaka-macbook <bazunaka32@gmail.com>
2022-12-01 13:02:49 +0300

    add code for FormRefer

--- Controllers/ReferencesController.java
+++ Controllers/ReferencesController.java
@@ -3,6 +3,9 @@ package Controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
 
 public class ReferencesController {
 
@@ -11,6 +14,21 @@ public class ReferencesController {
 
     @FXML
     private URL location;
+
+    @FXML
+    private Button addRecord;
+
+    @FXML
+    private Button deleteRecord;
+
+    @FXML
+    private Button editRecord;
+
+    @FXML
+    private TreeView<?> listReferences;
+
+    @FXML
+    private TableView<?> tableReferences;
 
     @FXML
     void initialize() {
