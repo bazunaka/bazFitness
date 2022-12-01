c966558a18776cbb763e050c41d10b06701f31e4
bazunaka-macbook <bazunaka32@gmail.com>
2022-12-01 17:24:38 +0300

    create and add code for  Account

--- Controllers/AccountController.java
+++ Controllers/AccountController.java
@@ -1,5 +1,51 @@
 package Controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class AccountController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField ageEdit;
+
+    @FXML
+    private TextField bicepsEdit;
+
+    @FXML
+    private TextField buttocksEdit;
+
+    @FXML
+    private TextField chestEdit;
+
+    @FXML
+    private TextField fatEdit;
+
+    @FXML
+    private TextField heightEdit;
+
+    @FXML
+    private TextField nameEdit;
+
+    @FXML
+    private TextField pressEdit;
+
+    @FXML
+    private TextField secondnameEdit;
+
+    @FXML
+    private TextField weightEdit;
+
+    @FXML
+    void initialize() {
+
+    }
 
 }
